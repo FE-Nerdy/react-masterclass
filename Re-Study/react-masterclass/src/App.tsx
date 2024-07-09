f4bb3e997ebed7ebaa73c6d8180c3b3ddd255210
SongJunHyuk <83599564+SongJunHyuk@users.noreply.github.com>
2024-07-09 22:40:34 +0900

    TypescriptLearning

---
+++ Re-Study/react-masterclass/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.h1`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
+
+function App() {
+  return (<Container>
+    <H1>
+      protected
+    </H1>
+  </Container>);
+}
+
+export default App;
